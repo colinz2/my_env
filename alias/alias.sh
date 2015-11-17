1bf44d623cf84cacae0649acabf75b4c290817a8
colin-zhang <ingm@foxmail.com>
2015-11-18 00:37:56 +0800

    alias,haha

---
+++ alias/alias.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+alias mv='mv -i '
+alias rm='rm -i '
+alias la='/bin/ls -aldF '
+alias lt='/bin/ls -ltr '
+alias h='history'
+alias cgrep='find . -name "*.*c" | xargs grep'
+alias hgrep='find . -name "*.h" | xargs grep'
+alias mgrep='find . -name "*mk*" | xargs grep'
+alias lgrep='find . -name "*log*" | xargs grep'
+alias view='vim -R'
+alias cdb='cd ~/bin'
+alias cde='cd ~/src'
---
+++ my_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ./alias/alias.sh
+echo "alias set"
