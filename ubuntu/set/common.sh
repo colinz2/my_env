f55d8c9bc77947ffb60454d9cf3753f893016cf2
colin-dvlp <colin_dvlp@qq.com>
2015-11-22 21:31:25 +0800

    add local dir

--- ubuntu/set/common.sh
+++ ubuntu/set/common.sh
@@ -1,8 +1,10 @@
 ulimit -c unlimited
 
-export DVLP_LIB=$DVLP_DIR/lib
-export DVLP_INC=$DVLP_DIR/inc
-export DVLP_BIN=$DVLP_DIR/bin
+export DVLP_LOCAL=$DVLP_DIR/local
+export DVLP_LIB=$DVLP_LOCAL/lib
+export DVLP_INC=$DVLP_LOCAL/include
+export DVLP_BIN=$DVLP_LOCAL/bin
+
 export DVLP_MK=$DVLP_DIR/mk
 
 export LD_LIBRARY_PATH=$DVLP_LIB
@@ -10,4 +12,4 @@ export PKG_CONFIG_PATH=$DVLP_LIB/pkgconfig
 
 export PATH=$PATH:$DVLP_MYENV
 export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$DVLP_DIR/bin
+export PATH=$PATH:$DVLP_BIN
