0a7d5ac45333b37a7306d58a3704237dfff53cf8
colin-dvlp <colin_dvlp@qq.com>
2015-11-22 20:04:17 +0800

    add go PATH setup

--- ubuntu/set/common.sh
+++ ubuntu/set/common.sh
@@ -1,13 +1,13 @@
 ulimit -c unlimited
 
-export DVLP_LIB=$DVLP/lib
-export DVLP_INC=$DVLP/inc
-export DVLP_BIN=$DVLP/bin
-export DVLP_MK=$DVLP/mk
+export DVLP_LIB=$DVLP_DIR/lib
+export DVLP_INC=$DVLP_DIR/inc
+export DVLP_BIN=$DVLP_DIR/bin
+export DVLP_MK=$DVLP_DIR/mk
 
 export LD_LIBRARY_PATH=$DVLP_LIB
 export PKG_CONFIG_PATH=$DVLP_LIB/pkgconfig
 
 export PATH=$PATH:$DVLP_MYENV
 export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$DVLP/bin
+export PATH=$PATH:$DVLP_DIR/bin
